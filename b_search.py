7e0f8094c8381aeca4425850b8681f2b0444813c
YeswanthRajakumartw <yeswanth.rajakumar@thoughtworks.com>
2023-07-27 12:55:05 +0530

    update: main.yml

---
+++ b_search.py
@@ -0,0 +1,26 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return -1
+
+if __name__ == "__main__":
+
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    target = 5
+
+    result = binary_search(arr, target)
+
+    if result != -1:
+        print(f"Element is present at index {result}")
+    else:
+        print("Element is not present in array")
