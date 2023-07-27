f375416e2be629b96bfdcce2b7828e5ffd8de619
YeswanthRajakumartw <yeswanth.rajakumar@thoughtworks.com>
2023-07-27 13:12:37 +0530

    update: py file

--- b_search.py
+++ b_search.py
@@ -1,7 +1,6 @@
 def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
-    
     while low <= high:
         mid = (low + high) // 2
         if arr[mid] == target:
@@ -11,13 +10,13 @@ def binary_search(arr, target):
             low = mid + 1
         else:
             high = mid - 1
-    
     return -1
 
-if __name__ ==  "__main__":
+
+if __name__ == "__main__":
 
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    target =  5
+    target = 5
 
     result = binary_search(arr, target)
 
